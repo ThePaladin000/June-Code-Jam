fe833765fee209b30703825ecaac0f6b9d6f8d95
Joshua Gomez <gomezjoshuam@gmail.com>
2025-06-06 11:44:59 -0400

    add navbar and footer components

--- June-Code-Jam/src/App.jsx
+++ June-Code-Jam/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <Navbar />
+      <div className="main-container">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
