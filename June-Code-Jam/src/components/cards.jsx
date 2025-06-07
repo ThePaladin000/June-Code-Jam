60b48aebbe90a0598ac192dea2053aa3ee807856
Rich <TonyRiches17@yahoo.com>
2025-06-07 15:55:17 -0400

    Style Cards1

--- June-Code-Jam/src/components/cards.jsx
+++ June-Code-Jam/src/components/cards.jsx
@@ -1,41 +1,53 @@
 export default function Cards() {
   return (
-    <div className="cards-container">
+    <div className="cards-containers">
+      <div className="card-container">
       <div className="card">
         <img
           src="https://placehold.co/600x400"
           alt="placeholder"
           className="card-image"
         />
-        <h1 className="card-title">Card 2</h1>
-        <h2 className="card-description">Card 2</h2>
       </div>
-      <div className="card">
-        <img
-          src="https://placehold.co/600x400"
-          alt="placeholder"
-          className="card-image"
-        />
-        <h1 className="card-title">Card 2</h1>
-        <h2 className="card-description">Card 2</h2>
+      <div>
+        <h2 className="card-description">Card Location 1</h2>
       </div>
-      <div className="card">
-        <img
-          src="https://placehold.co/600x400"
-          alt="placeholder"
-          className="card-image"
-        />
-        <h1 className="card-title">Card 3</h1>
-        <h2 className="card-description">Card 3</h2>
       </div>
+      <div className="card-container">
       <div className="card">
         <img
           src="https://placehold.co/600x400"
           alt="placeholder"
           className="card-image"
         />
-        <h1 className="card-title">Card 4</h1>
-        <h2 className="card-description">Card 4</h2>
+      </div>
+      <div>
+        <h2 className="card-description">Card Location 2</h2>
+      </div>
+      </div>
+      <div className="card-container">
+      <div className="card">
+        <img
+          src="https://placehold.co/600x400"
+          alt="placeholder"
+          className="card-image"
+        />
+      </div>
+      <div>
+        <h2 className="card-description">Card Location 3</h2>
+      </div>
+      </div>
+      <div className="card-container">
+      <div className="card">
+        <img
+          src="https://placehold.co/600x400"
+          alt="placeholder"
+          className="card-image"
+        />
+      </div>
+      <div>
+        <h2 className="card-description">Card Location 4</h2>
+      </div>
       </div>
     </div>
   );
