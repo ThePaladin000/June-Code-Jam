f988c39849cc148ae2a67971a21af350003016e5
Majestyk Vaughns <Majestyk.vaughns@gmail.com>
2025-06-11 00:50:06 -0500

    add firebase configs into .env for best practice

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-     apiKey: "AIzaSyDSy34GDmkJpkhz3m9gJMob5pqpmOXaAz4",
-     authDomain: "june-code-jam-b0ad5.firebaseapp.com",
-     projectId: "june-code-jam-b0ad5",
-     storageBucket: "june-code-jam-b0ad5.firebasestorage.app",
-     messagingSenderId: "1005830784982",
-     appId: "1:1005830784982:web:fcf6c32e6aab7846f20677"
-    };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
-    const app = initializeApp(firebaseConfig);
-
-    export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
